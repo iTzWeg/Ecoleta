0e37b41a1aa6364b2ea5910d9c7115156906eb7c
Wellington de Andrade <weg.andrade@gmail.com>
2020-06-07 06:02:31 -0300

    Criada tela de detalhes do ponto e funcionidade de enviar email e whatsapp para o estabelecimento do ponto de coleta

--- mobile/src/Pages/Detail/index.tsx
+++ mobile/src/Pages/Detail/index.tsx
@@ -1,13 +1,59 @@
-import React from 'react';
-import {View, StyleSheet,TouchableOpacity, Text,Image, SafeAreaView} from 'react-native';
+import React, {useEffect,useState} from 'react';
+import {View, StyleSheet,TouchableOpacity, Text,Image, SafeAreaView,Linking} from 'react-native';
 import { Feather as Icon, FontAwesome} from '@expo/vector-icons';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import {RectButton} from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
+import api from '../../services/api';
 
+
+interface Params {
+  point_id: number;
+}
+
+interface Data{
+  point: {
+    image:string,
+    name: string,
+    email: string,
+    whatsapp: string,
+    city: string,
+    uf: string,
+  };
+  items:{
+    title: string
+  }[];
+}
 const Detail = () => {
+    const route = useRoute();
+    const [data, setData] = useState<Data>({} as Data);
     const navigation = useNavigation();
+    const routeParams = route.params as Params;
+    
+    useEffect(() => {
+      api.get(`points/${routeParams.point_id}`)
+      .then(reponse =>{
+          setData(reponse.data);
+      });
+    },[])
+
     function handleNavigateBack(){
         navigation.goBack();
+    }
+    function handleComposeMail(){
+      MailComposer.composeAsync({
+        subject: 'Interesse na coleta de resíduos',
+        recipients: [data.point.email],
+      })
+    }
+
+    function handleComposeWhatsapp(){
+        Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}
+        &text = Tenho Interesse na coleta de resíduos`);
+    }
+
+    if(!data.point){
+      return null
     }
     return (
     <SafeAreaView style={{flex:1}}>
@@ -16,23 +62,25 @@ const Detail = () => {
                 <Icon name="arrow-left" size={20} color="#34cb79" />
             </TouchableOpacity>
 
-            <Image style={styles.pointImage} source={{uri:"https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=80&q=60"}}></Image>
-            <Text style={styles.pointName}>Mercado</Text>
-            <Text style={styles.pointItems}>Lámpadas e óleo de cozinha</Text>
+            <Image style={styles.pointImage} source={{uri:data.point.image}}></Image>
+            <Text style={styles.pointName}>{data.point.name}</Text>
+            <Text style={styles.pointItems}>{
+              data.items.map(item =>item.title).join(', ')}
+            </Text>
 
             <View style={styles.address}>
-                <Text style={styles.addressTitle}>Endereço</Text>
-                <Text style={styles.addressContent}>Jundiaí</Text>
-                
-
+              <Text style={styles.addressTitle}>Endereço</Text>
+              <Text style={styles.addressContent}>
+                {`${data.point.city}, ${data.point.uf}`}
+              </Text>
             </View>
         </View>
         <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={handleComposeWhatsapp}>
                 <FontAwesome name="whatsapp" size={20} color="#FFF"/>
                 <Text style={styles.buttonText}>Whatsapp</Text>
             </RectButton>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={handleComposeMail}>
                 <Icon name="mail" size={20} color="#FFF"/>
                 <Text style={styles.buttonText}>Mail</Text>
             </RectButton>
@@ -112,7 +160,7 @@ const styles = StyleSheet.create({
     buttonText: {
       marginLeft: 8,
       color: '#FFF',
-      fontSize: 16,
+      fontSize: 17,
       fontFamily: 'Roboto_500Medium',
     },
   });
